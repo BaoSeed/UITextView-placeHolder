8a6f49f3ecf5649b30156110e6ca7610324b11e1
朱鹏 <zhupeng@Ivan-MacBook-Pro.local>
2017-07-01 15:13:28 +0800

    image size

--- UITextView_placeHolder/UITextView_placeHolder/ViewController.m
+++ UITextView_placeHolder/UITextView_placeHolder/ViewController.m
@@ -19,6 +19,53 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://app.qianhtj.com/app_img/appPic/TJ20170606100399_1496740048498.jpg"]];
+    
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc]init] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        
+        
+        
+        //@"image/jpeg"
+        NSString *MIMEType = response.MIMEType;
+        //615203
+        long long expectedContentLength = response.expectedContentLength;
+        //TJ20170606100399_1496740048498.jpg
+        NSString *suggestedFilename =  response.suggestedFilename;
+        //
+        NSHTTPURLResponse *res = (NSHTTPURLResponse *)response;
+        
+        NSDictionary *allHeaderFields = res.allHeaderFields;
+        
+        NSLog(@"%@",allHeaderFields);
+        /*
+        {
+            "Accept-Ranges"     = bytes;
+            Connection          = "keep-alive";
+            "Content-Length"    = 615203;
+            "Content-Type"      = "image/jpeg";
+            Date                = "Sat, 01 Jul 2017 06:26:35 GMT";
+            Etag                = "\"593670d0-96323\"";
+            "Last-Modified"     = "Tue, 06 Jun 2017 09:07:28 GMT";
+            Server              = nginx;
+        }
+         */
+        
+        
+        CGSize size    =  [self JPGImageSizeWithRangeHeader:data];
+        
+        NSLog(@"%@",NSStringFromCGSize(size));
+        
+        CGSize pngSize = [self PNGImageSizeWithRangeHeader:data];
+        
+        NSLog(@"%@",NSStringFromCGSize(pngSize));
+        
+        CGSize gifSize = [self GIFImageSizeWithRangeHeader:data];
+        
+        NSLog(@"%@",NSStringFromCGSize(gifSize));
+        
+    }];
+    
 
     dispatch_semaphore_t semaphore1 = dispatch_semaphore_create(0);
     dispatch_semaphore_t semaphore2 = dispatch_semaphore_create(0);
@@ -50,6 +97,98 @@
 
 
 
+- (CGSize)GIFImageSizeWithRangeHeader:(NSData *)data{
+    short w1 = 0, w2 = 0;
+    [data getBytes:&w1 range:NSMakeRange(0, 1)];
+    [data getBytes:&w2 range:NSMakeRange(1, 1)];
+    short w = w1 + (w2 << 8);
+    
+    short h1 = 0, h2 = 0;
+    [data getBytes:&h1 range:NSMakeRange(2, 1)];
+    [data getBytes:&h2 range:NSMakeRange(3, 1)];
+    short h = h1 + (h2 << 8);
+    return CGSizeMake(w, h);
+}
+
+- (CGSize)PNGImageSizeWithRangeHeader:(NSData *)data{
+    int w1 = 0, w2 = 0, w3 = 0, w4 = 0;
+    [data getBytes:&w1 range:NSMakeRange(0, 1)];
+    [data getBytes:&w2 range:NSMakeRange(1, 1)];
+    [data getBytes:&w3 range:NSMakeRange(2, 1)];
+    [data getBytes:&w4 range:NSMakeRange(3, 1)];
+    int w = (w1 << 24) + (w2 << 16) + (w3 << 8) + w4;
+    int h1 = 0, h2 = 0, h3 = 0, h4 = 0;
+    [data getBytes:&h1 range:NSMakeRange(4, 1)];
+    [data getBytes:&h2 range:NSMakeRange(5, 1)];
+    [data getBytes:&h3 range:NSMakeRange(6, 1)];
+    [data getBytes:&h4 range:NSMakeRange(7, 1)];
+    int h = (h1 << 24) + (h2 << 16) + (h3 << 8) + h4;
+    
+    return CGSizeMake(w, h);
+}
+
+- (CGSize)JPGImageSizeWithRangeHeader:(NSData *)data{
+    
+    
+    if ([data length] <= 0x58) {
+        return CGSizeZero;
+    }
+    
+    if ([data length] < 210) {// 肯定只有一个DQT字段
+        
+        short w1 = 0, w2 = 0;
+        [data getBytes:&w1 range:NSMakeRange(0x60, 0x1)];
+        [data getBytes:&w2 range:NSMakeRange(0x61, 0x1)];
+        short w = (w1 << 8) + w2;
+        short h1 = 0, h2 = 0;
+        
+        [data getBytes:&h1 range:NSMakeRange(0x5e, 0x1)];
+        [data getBytes:&h2 range:NSMakeRange(0x5f, 0x1)];
+        short h = (h1 << 8) + h2;
+        return CGSizeMake(w, h);
+        
+    } else {
+        
+        short word = 0x0;
+        [data getBytes:&word range:NSMakeRange(0x15, 0x1)];
+        
+        if (word == 0xdb) {
+            
+            [data getBytes:&word range:NSMakeRange(0x5a, 0x1)];
+            if (word == 0xdb) {// 两个DQT字段
+                
+                short w1 = 0, w2 = 0;
+                [data getBytes:&w1 range:NSMakeRange(0xa5, 0x1)];
+                [data getBytes:&w2 range:NSMakeRange(0xa6, 0x1)];
+                short w = (w1 << 8) + w2;
+                
+                short h1 = 0, h2 = 0;
+                [data getBytes:&h1 range:NSMakeRange(0xa3, 0x1)];
+                [data getBytes:&h2 range:NSMakeRange(0xa4, 0x1)];
+                short h = (h1 << 8) + h2;
+                return CGSizeMake(w, h);
+                
+            } else {// 一个DQT字段
+                
+                short w1 = 0, w2 = 0;
+                [data getBytes:&w1 range:NSMakeRange(0x60, 0x1)];
+                [data getBytes:&w2 range:NSMakeRange(0x61, 0x1)];
+                short w = (w1 << 8) + w2;
+                short h1 = 0, h2 = 0;
+                
+                [data getBytes:&h1 range:NSMakeRange(0x5e, 0x1)];
+                [data getBytes:&h2 range:NSMakeRange(0x5f, 0x1)];
+                short h = (h1 << 8) + h2;
+                return CGSizeMake(w, h);
+                
+            }
+        }
+        
+        else {
+            return CGSizeZero;
+        }
+    }
+}
 
 
 
